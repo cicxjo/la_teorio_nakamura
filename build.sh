e391572100ae803a23df9ddb475d74074fbab351
Cyril Augier <cicxjo@posteo.net>
2023-12-22 15:37:45 +0100

    add course 1 for ‘fr’

--- build.sh
+++ build.sh
@@ -16,3 +16,17 @@ build()
 	$script fr < "leciono-$2.txt" > "$dir/$1/$2.tsv.txt" 2>&1
 	$filter < "$dir/$1/$2.tsv.txt" > "$dir/$1/$2.cards.txt"
 }
+
+build_fr()
+{
+	mkdir -p $dir/fr
+
+	i=0
+	while [ $i -ne 1 ]
+	do
+		i=$((i + 1))
+		build fr $i
+	done
+}
+
+build_fr
